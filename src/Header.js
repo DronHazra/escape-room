a1d0023bb560026f98a420069331105386940171
Dron <dronh.to@gmail.com>
2021-01-29 20:11:30 -0500

    fix line break

--- src/Header.js
+++ src/Header.js
@@ -7,13 +7,14 @@ import SmashEffect from './SmashEffect';
 export default function Header() {
 	const smash = useContext(SFXContext);
 	return (
-		<Typography variant='h2'>
+		<Typography variant='h2' align='center'>
 			<WindupChildren>
 				{'Welcome to '}
 				<Pause ms={500} />
 				{'... '}
 				<Pause ms={500} />
 				<SmashEffect />
+				<br />
 				{'Space Escape!'}
 			</WindupChildren>
 		</Typography>
