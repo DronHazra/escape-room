57171e7dc81b5be1b8989ceda614176e8df7b8ff
Dron <dronh.to@gmail.com>
2021-02-02 16:25:56 -0500

    added a scroll follower

--- src/Dialog.js
+++ src/Dialog.js
@@ -8,6 +8,7 @@ import {
 	Typography,
 	Grid,
 } from '@material-ui/core';
+import useComponentSize from '@rehooks/component-size';
 import React, {
 	useState,
 	useRef,
@@ -17,6 +18,7 @@ import React, {
 	Children,
 	createContext,
 } from 'react';
+import { useInView } from 'react-intersection-observer';
 import theme from './theme';
 
 const useStyles = makeStyles(theme => {
@@ -32,9 +34,49 @@ export const DialogChildContext = createContext({
 	proceed: () => {},
 });
 
+function useKeepInViewer(height) {
+	const measurementRef = useRef();
+	const prevHeightRef = useRef(height);
+
+	const [inViewRef, isInView] = useInView({
+		rootMargin: '-100px 0px',
+	});
+	const setRef = useCallback(
+		node => {
+			measurementRef.current = node;
+			inViewRef(node);
+		},
+		[inViewRef]
+	);
+	useEffect(() => {
+		if (!measurementRef.current) {
+			return;
+		}
+
+		const scrollBottom = window.scrollY + window.innerHeight;
+		const bottomPos =
+			measurementRef.current.offsetTop +
+			measurementRef.current.offsetHeight;
+		const isJustOutOfView = Math.abs(scrollBottom - bottomPos) < 200;
+		if (!isInView && height !== prevHeightRef.current && isJustOutOfView) {
+			window.scroll({
+				top:
+					measurementRef.current.offsetTop -
+					(window.innerHeight / 3) * 2,
+				behavior: 'smooth',
+			});
+		}
+	}, [isInView, height]);
+
+	return <div ref={setRef} />;
+}
+
 export default function Dialog({ children }) {
 	const [numberOfCards, setNumberOfCards] = useState(1);
 	const activeChildIndex = numberOfCards - 1;
+	const rootRef = useRef(null);
+	const { height } = useComponentSize(rootRef);
+	const keepy = useKeepInViewer(height);
 
 	const shownChildren = Children.toArray(children)
 		.slice(0, numberOfCards)
@@ -57,9 +99,12 @@ export default function Dialog({ children }) {
 		});
 	return (
 		<Container maxWidth='sm'>
-			<Grid container direction='column' spacing={3}>
-				{shownChildren}
-			</Grid>
+			<div ref={rootRef}>
+				<Grid container direction='column' spacing={3}>
+					{shownChildren}
+				</Grid>
+				{keepy}
+			</div>
 		</Container>
 	);
 }
